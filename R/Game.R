2026f085f1e4a4a1fcb638d27514cae1f9fe925f
WojciechKretowicz <wojtekkretowicz@gmail.com>
2019-05-15 11:22:42 +0200

    Dokumentacja

--- R/Game.R
+++ R/Game.R
@@ -1,3 +1,12 @@
+#' @title Guess Game!
+#'
+#' @description You can now play your favourite game! There is randomly generated integer in range [1,range]. You have some number of trials. Game will tell if your guess is greater or smaller than the hidden number.
+#'
+#' @param range Range of generated inter
+#' 
+#' @param trials Number of your trials
+#'
+#' @export
 Game <- function(range=10, trials=3) {
   x <- sample(1:range, 1)
   i <- 1
